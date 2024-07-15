72039b023fed88ae783c935e22da96ada20c871e
praveen kumar <praveenk15499@gmail.com>
2024-07-16 00:53:28 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,79 @@
+# Node.js Number Processing API for demo assignment
+
+## Requirements
+A client is using a dashboard. He inputs a number from 1 to 25 {For eg - 5}. That number automatically gets multiplied by 7. 
+1) If final number after multiplied by 7, is greater than 140, then its goes to file A. 
+2) If final number after multiplied by 7, is greater than 100, then its goes to file B
+3) If final number after multiplied by 7, is greater than 60 then its goes to file C. 
+4) And all other numbers goes to file D
+
+Once a number is stored in all files (A to D), the user can’t enter any new number and the process gets complete 
+
+
+## Features
+
+- Uses `morgan` for logging requests with rotating file streams.
+- Limits incoming requests to prevent abuse.
+- Provides an endpoint to view stored numbers.
+
+## Setup
+
+```bash
+git clone nodejs-assignment
+cd nodejs-assignment
+npm install
+mkdir logs output
+npm run dev
+```
+
+## API Endpoints
+### Health Check
+
+1. GET /
+- Description: Check if the server is running.
+- Response:
+`
+Server is up & working!!
+`
+
+2. POST /input
+
+- Description: Process an input number, multiply by 7, and store the result in the appropriate file.
+- Request Body:
+```json
+{
+  "number": <integer between 1 and 25>
+}
+```
+- Responses:
+```
+400 Bad Request: If the number is not between 1 and 25.
+200 OK: Number processed and saved, or process is complete if all files are filled
+```
+
+3. GET /files
+
+- Description: Retrieve all stored numbers from the files.
+- Response:
+```json
+{
+  "A": [<number1>, <number2>, ...],
+  "B": [<number1>, <number2>, ...],
+  "C": [<number1>, <number2>, ...],
+  "D": [<number1>, <number2>, ...]
+}
+```
+
+## Example usage
+
+* Send a number to be processed
+```curl
+curl -X POST http://localhost:3000/input -H "Content-Type: application/json" -d '{"number": 5}'
+```
+
+* Get stored numbers:
+
+```
+curl http://localhost:3000/files
+```
+
---
+++ app.js
@@ -0,0 +1,98 @@
+import dotenv from "dotenv";
+import express from "express";
+import bodyParser from "body-parser";
+import fs from "fs";
+import rateLimit from "express-rate-limit";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
+import { createStream } from "rotating-file-stream";
+
+dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+// Rate limiting is enabled by default
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 10, // limit each IP to 10 requests per windowMs
+});
+app.use(limiter);
+
+// logs and output directories
+const logsDir = path.join(__dirname, "logs"); // storing access logs for debugging
+const outputDir = path.join(__dirname, "output"); // for writing output files
+if (!fs.existsSync(logsDir)) fs.mkdirSync(logsDir); // check for existing directory
+if (!fs.existsSync(outputDir)) fs.mkdirSync(outputDir);
+
+// Morgan is used for logging with rotating file stream for keeping files short.
+const accessLogStream = createStream("access.log", {
+  interval: "1d", // rotate daily
+  path: logsDir,
+});
+app.use(morgan("combined", { stream: accessLogStream }));
+
+let files = {
+  A: [],
+  B: [],
+  C: [],
+  D: [],
+};
+
+app.get("/", (req, res) => {
+  res.send("Server is up & working!!");
+});
+
+app.post("/input", (req, res, next) => {
+  try {
+    const { number } = req.body;
+    if (number < 1 || number > 25) {
+      return res
+        .status(400)
+        .send("Number must be between 1 and 25 and must be a valid number");
+    }
+
+    let result = number * 7;
+    if (result > 140) {
+      files.A.push(result);
+    } else if (result > 100) {
+      files.B.push(result);
+    } else if (result > 60) {
+      files.C.push(result);
+    } else {
+      files.D.push(result);
+    }
+
+    if (files.A.length && files.B.length && files.C.length && files.D.length) {
+      fs.writeFileSync(path.join(outputDir, "A.json"), JSON.stringify(files.A));
+      fs.writeFileSync(path.join(outputDir, "B.json"), JSON.stringify(files.B));
+      fs.writeFileSync(path.join(outputDir, "C.json"), JSON.stringify(files.C));
+      fs.writeFileSync(path.join(outputDir, "D.json"), JSON.stringify(files.D));
+      return res.send("All files are filled. Process is complete.");
+    }
+
+    res.send("Number processed and saved.");
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.get("/files", (req, res) => {
+  res.json(files);
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something went wrong!");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
